0e2bbc8aab15db034a340abd2b489b3484f3c9a2
brenoalmeidaa07 <albeidabreno15@gmail.com>
2024-04-26 15:05:51 -0300

    meu primeiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -11,8 +13,8 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          Read <Link href="/posts/first-post">this page!</Link>
+          </h1>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
